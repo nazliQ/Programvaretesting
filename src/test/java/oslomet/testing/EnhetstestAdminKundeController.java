cf23c7c1649842da6936bd83f452105786630001
jepnik <jepnikast@gmail.com>
2024-01-23 14:33:46 +0100

    Testet hentAlle

--- src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -6,7 +6,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import oslomet.testing.API.AdminKundeController;
 import oslomet.testing.API.BankController;
+import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Models.Konto;
 import oslomet.testing.Models.Kunde;
@@ -23,4 +25,45 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EnhetstestAdminKundeController {
+
+	@InjectMocks
+	private AdminKundeController adminKundeController;
+
+	@Mock
+	private AdminRepository adminRepository;
+
+	@Mock
+	private Sikkerhet sjekk;
+
+	@Test
+	public void test_hentAlle_loggetInn() {
+		//Arrange
+		List<Kunde> kundeListe = new ArrayList<>();
+		Kunde kunde1 = new Kunde("12345678910", "Jens", "Knutsen",
+				"Veienveien98", "9787", "Buskerud", "8976576", "passord123");
+		kundeListe.add(kunde1);
+
+		Mockito.when(sjekk.loggetInn()).thenReturn("12345678910");
+
+		Mockito.when(adminRepository.hentAlleKunder()).thenReturn(kundeListe);
+
+		//Act
+		List<Kunde> resultat =adminKundeController.hentAlle();
+
+		//Assert
+		assertEquals(kundeListe, resultat);
+	}
+
+	@Test
+	public void test_hentAlle_ikkeLoggetInn() {
+		//Arrange
+		Mockito.when(sjekk.loggetInn()).thenReturn(null);
+
+		//Act
+		List<Kunde> resultat = adminKundeController.hentAlle();
+
+		//Assert
+		assertNull(resultat);
+	}
+
 }
