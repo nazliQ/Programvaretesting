71541f1a5d1d2f62c9f476d0650fbd1419ca291c
Hallvard96 <hallvard.berger@gmail.com>
2024-02-29 14:09:11 +0100

    Enhetstest er OK Integrasjonstest i gang

--- src/main/java/oslomet/testing/API/AdminKontoController.java
+++ src/main/java/oslomet/testing/API/AdminKontoController.java
@@ -6,6 +6,7 @@ import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.Models.Konto;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @RestController
@@ -52,5 +53,11 @@ public class AdminKontoController {
            return repository.slettKonto(kontonummer);
         }
         return "Ikke innlogget";
+    }
+    @Autowired
+    private DataSource dataSource;
+    @GetMapping("/initDB")
+    public String initDB(){
+        return repository.initDB(dataSource);
     }
 }
